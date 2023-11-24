a654a0f2c4a652dc92e638c5fff77d64c6e1bfc6
Michael Liao <askxuefeng@gmail.com>
2023-11-24 08:15:40 +0800

    rename post/pre build

--- layout/default/post-build.mjs
+++ layout/default/post-build.mjs
@@ -7,7 +7,6 @@ function run(cmd) {
 
 export default async function (config, output) {
     console.log(`post-build: cwd = ${process.cwd()}, output = ${output}`);
-    run(`mkdir -p ${output}/static`);
-    run(`cp ./static/* ${output}/static/`);
+    run(`cp static/* ${output}/`);
     run(`npx tailwindcss -i ./custom-tailwind.txt -o ${output}/static/custom-tailwind.css`);
 };
