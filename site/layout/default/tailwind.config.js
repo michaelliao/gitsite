d5991042ac2517fcb0023a0ad8ae0a11318daf2e
Michael Liao <askxuefeng@gmail.com>
2023-12-13 18:40:51 +0800

    rename css

--- site/layout/default/tailwind.config.js
+++ site/layout/default/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: 'class',
-    content: ['./custom-tailwind.css', './**/*.html'],
+    content: ['./default-tailwind.css', './**/*.html'],
     theme: {
         extend: {},
     },
