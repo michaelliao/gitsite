e13eeb03fb1935f96bbe90dc505cc23c8e25b59a
Michael Liao <askxuefeng@gmail.com>
2024-01-12 22:08:28 +0800

    remove theme

--- themes/default/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    darkMode: 'class',
-    content: ['./**/*.html'],
-    theme: {
-        extend: {},
-    },
-    plugins: ['forms', 'typography', 'aspect-ratio'],
-}
